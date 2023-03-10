8d3c1ecedc932fb96e6e90ce97eab2e0edcfd3f5
Dandi Rizky <dandirizkyy94@gmail.com>
2023-03-10 16:49:34 +0700

    test gitignore

---
+++ test/test.js
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+
+const questions = [
+
+
+]
+
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    console.log(JSON.stringify(answers, null, 2));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("Your console environment is not supported!");
+    } else {
+      console.log(error);
+    }
+  });
